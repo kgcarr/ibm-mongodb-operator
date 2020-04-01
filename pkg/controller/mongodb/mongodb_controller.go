574ac21f8642e4c768ca64d870b40a99e9ea32b2
Kevin G. Carr <kgcarr@us.ibm.com>
2020-04-01 08:00:00 -0500

    build install configmap

--- pkg/controller/mongodb/mongodb_controller.go
+++ pkg/controller/mongodb/mongodb_controller.go
@@ -23,7 +23,7 @@ import (
 	"text/template"
 	"time"
 
-	builder "github.com/ibm/ibm-auditlogging-operator/pkg/resources"
+	builder "github.com/ibm/ibm-mongodb-operator/pkg/resources"
 	"github.com/ghodss/yaml"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
