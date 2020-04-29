bff8119fe50549e20ff7b05a740b6d28f92630e7
Kevin G. Carr <kgcarr@us.ibm.com>
2020-04-28 22:49:49 -0500

    add current object info to update

--- pkg/controller/mongodb/mongodb_controller.go
+++ pkg/controller/mongodb/mongodb_controller.go
@@ -358,8 +358,7 @@ func (r *ReconcileMongoDB) createFromYaml(instance *operatorv1alpha1.MongoDB, ya
 		currentObject := &unstructured.Unstructured{}
 		r.client.Get(context.TODO(), client.ObjectKey{
 			Namespace: instance.Namespace,
-			Name: obj.GetName(),
-			Kind: obj.GetKind()}, currentObject)
+			Name: obj.GetName()}, currentObject)
 		obj.SetCreationTimestamp(currentObject.GetCreationTimestamp())
 		obj.SetOwnerReferences(currentObject.GetOwnerReferences())
 		obj.SetResourceVersion(currentObject.GetResourceVersion())
